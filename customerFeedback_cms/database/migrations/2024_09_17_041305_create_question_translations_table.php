66629e79915f7b8d236eb5e7a3a0e261b29bed1d
Haroon-aygtc <138967381+Haroon-aygtc@users.noreply.github.com>
2024-10-04 15:02:06 +0400

    customer feedback merge backend and frontend

---
+++ customerFeedback_cms/database/migrations/2024_09_17_041305_create_question_translations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('question_translations', function (Blueprint $table) {
+          $table->id(); // Primary key
+          $table->foreignId('question_id')->constrained()->onDelete('cascade');
+          $table->string('locale'); // e.g., 'en', 'ar'
+          $table->string('title');
+          $table->text('description');
+          $table->string('question_text');
+          $table->timestamps();
+
+          $table->unique(['question_id', 'locale']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('question_translations');
+    }
+};
